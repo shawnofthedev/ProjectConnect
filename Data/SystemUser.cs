b7479a07debc364a393befc4d192fedf8339d060
shawnofthedev <svotaw81@gmail.com>
2019-11-16 02:52:54 -0500

    Add project files.

---
+++ Data/SystemUser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectConnect.Data
+{
+    public class SystemUser
+    {
+        [Key]
+        public Guid SystemUserId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string ContactNumber { get; set; }
+
+        public string AddressLine1 { get; set; }
+
+        public string AddressLine2 { get; set; }
+
+        public string City { get; set; }
+ 
+        public ICollection<State> States { get; set; }
+ 
+        [DataType(DataType.PostalCode)]
+        public string Zip { get; set; } 
+    }
+
+    public class State
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string StateAbbrev { get; set; }
+
+        public string StateName { get; set; }
+
+
+    }
+}
